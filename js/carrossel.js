fb1f445398329510fcb72eb47570126728842555
Lucas Petrasso <lucaspetrasso@hotmail.com>
2026-07-14 14:36:42 -0300

    Criação do carrossel na página about_us e atualização do layout

---
+++ js/carrossel.js
@@ -0,0 +1,37 @@
+const slides = document.querySelector(".carrossel_slides");
+const imagens = document.querySelectorAll(".carrossel_slides img");
+
+const btnNext = document.querySelector(".carrossel_btn_next");
+const btnPrev = document.querySelector(".carrossel_btn_prev");
+
+let indice = 0;
+
+btnNext.addEventListener("click", () => {
+
+    indice++;
+
+    if(indice >= imagens.length){
+        indice = 0;
+    }
+
+    atualizarSlide();
+
+});
+
+btnPrev.addEventListener("click", () => {
+
+    indice--;
+
+    if(indice < 0){
+        indice = imagens.length - 1;
+    }
+
+    atualizarSlide();
+
+});
+
+function atualizarSlide(){
+
+    slides.style.transform = `translateX(-${indice * 100}%)`;
+
+}

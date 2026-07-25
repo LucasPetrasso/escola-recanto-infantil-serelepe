8a67024024d0bd4be5a6527c6161aa2cf56d84fb
Lucas Petrasso <lucaspetrasso@hotmail.com>
2026-07-25 16:21:12 -0300

    Implementação do JavaScript para navegação dos cards da página index.html

--- js/navigation.js
+++ js/navigation.js
@@ -3,3 +3,21 @@ const btnAbout = document.querySelector("#btn_about");
 btnAbout.addEventListener("click", () => {
     window.location.href = "/pages/about_us.html";
 });
+
+const cardBercario = document.querySelector("#bercario_card");
+
+cardBercario.addEventListener("click", () => {
+    window.location.href = "/pages/turmas.html#bercario";
+});
+
+const cardMaternal = document.querySelector("#maternal_card");
+
+cardMaternal.addEventListener("click", () => {
+    window.location.href = "/pages/turmas.html#maternal";
+});
+
+const cardEtapa = document.querySelector("#etapa_card");
+
+cardEtapa.addEventListener("click", () => {
+    window.location.href = "/pages/turmas.html#etapa";
+});

7c1dc4645df19ff7d351170bdc9087bc7298eec2
Lucas Petrasso <lucaspetrasso@hotmail.com>
2026-08-25 14:27:53 -0300

    Correção de caminhos relativos dos cards de turmas na página index.html

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,23 +1,23 @@
 const btnAbout = document.querySelector("#btn_about");
 
 btnAbout.addEventListener("click", () => {
-    window.location.href = "/pages/about_us.html";
+    window.location.href = "./pages/about_us.html";
 });
 
 const cardBercario = document.querySelector("#bercario_card");
 
 cardBercario.addEventListener("click", () => {
-    window.location.href = "/pages/turmas.html#bercario";
+    window.location.href = "./pages/turmas.html#bercario";
 });
 
 const cardMaternal = document.querySelector("#maternal_card");
 
 cardMaternal.addEventListener("click", () => {
-    window.location.href = "/pages/turmas.html#maternal";
+    window.location.href = "./pages/turmas.html#maternal";
 });
 
 const cardEtapa = document.querySelector("#etapa_card");
 
 cardEtapa.addEventListener("click", () => {
-    window.location.href = "/pages/turmas.html#etapa";
+    window.location.href = "./pages/turmas.html#etapa";
 });
